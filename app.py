b1b32474756d75890b8c7025cbf73aa7a98d26d5
Sarthak <sarthak.cmk.al@gmail.com>
2024-10-23 18:23:33 +0530

    last dance

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ def ask_question_api():
 # Run the Flask app
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5001))
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0') 
